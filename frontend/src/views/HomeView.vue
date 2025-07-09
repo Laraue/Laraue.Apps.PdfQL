743aa2f6c9cb7607c992d2cf68b02b414cf4e026
DESKTOP-KT6GSMQ\Ilya <win7user10@yandex.ru>
2025-07-09 22:04:33 +0300

    friendlier frontend

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -9,16 +9,41 @@
       <div class="content">
         <div class="title">
           <div class="title-text">
-            Extract tables from PDF
+            Extract objects from PDF
           </div>
           <div class="title-description">
-            The service with it's own language helps you extract data in any format you want. Free and powerful with API's and more more features.
-          </div>
-          <div class="title-documentation-link">
-            <a target="_blank" href="https://github.com/win7user10/Laraue.PdfQL">Documentation</a>
+            The service helps to extract PDF parts in the popular formats for free. No even registration required.
           </div>
         </div>
-        <div class="editor-frame">
+        <div class="snippet-selection-frame">
+          <div class="snippet-selection-dropdown">
+            <el-dropdown>
+              <el-button>
+                {{ currentMode ? currentMode.title : 'I want to' }}<el-icon class="el-icon--right"><arrow-down /></el-icon>
+              </el-button>
+              <template #dropdown>
+                <el-dropdown-menu>
+                  <el-dropdown-item v-for="mode in modes" @click="chooseSuggestion(mode.id)">{{ mode.title }}</el-dropdown-item>
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
+          </div>
+        </div>
+        <div class="select-pdf-frame" v-if="currentMode">
+          <el-upload
+              class="pdf-uploader"
+              :on-change="handleChange"
+              :auto-upload="false"
+              accept="application/pdf"
+              drag
+              :limit="2">
+            <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+            <div class="el-upload__text">
+              Drop PDF here to process or <em>click to upload</em>
+            </div>
+          </el-upload>
+        </div>
+        <div class="editor-frame" v-if="currentMode?.id === 'manual'">
           <div class="editor-frame-left">
             <div class="editor-code-frame">
               <PdfqlEditor
@@ -29,27 +54,19 @@
               Enter PDF query or
               <a href="" @click.prevent="isSnippetWindowOpened = true">choose</a>
               a snippet.
+              Full <a target="_blank" href="https://github.com/win7user10/Laraue.PdfQL">Documentation</a> is available on github.
             </div>
           </div>
-          <div class="editor-frame-right">
-            <el-upload
-                class="pdf-uploader"
-                :on-change="handleChange"
-                :auto-upload="false"
-                :limit="1">
-              <a href="#">Select PDF</a>
-            </el-upload>
-          </div>
         </div>
-        <div>
-          <el-button
+        <div class="run-execution-frame">
+          <el-button v-if="pdfql.length > 0 && file"
+            :loading="isLoading"
             class="execute-pdfql-button"
-            @click="run"
-            :disabled="pdfql.length == 0 || file == null">
-            Execute PDF query
+            @click="run">
+            {{ isLoading ? 'Processing' : 'Start' }}
           </el-button>
         </div>
-        <div class="execution-result-frame">
+        <div class="execution-result-frame" v-if="result.errors.length > 0 || result.result">
           <div class="execution-result">
            <div v-for="error in result.errors" class="execution-result__item">
              <p class="execution-result__error-line">
@@ -103,18 +120,19 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, onMounted, ref, watch} from 'vue';
-import axios from "axios";
+import {computed, defineComponent, onMounted, ref, watch} from 'vue';
+import axios, {AxiosError} from "axios";
 import PdfqlEditor from "@/components/PdfqlEditor.vue";
 import {PdfqlError} from "@/components/PdfqlError";
-import { Plus } from '@element-plus/icons-vue'
+import { ArrowDown, UploadFilled } from '@element-plus/icons-vue'
 import {UploadFile, UploadProps} from "element-plus";
 
 export default defineComponent({
   name: 'HomeView',
   components: {
     PdfqlEditor,
-    Plus
+    ArrowDown,
+    UploadFilled
   },
   setup(){
     const defaultPsql = `select(tables)`;
@@ -135,7 +153,18 @@ export default defineComponent({
     }
 
     const run = async () => {
-      result.value = await runQueryAsync();
+      try {
+        result.value = await runQueryAsync();
+      }
+      catch (e) {
+        result.value.result = null;
+        if (e instanceof AxiosError) {
+          result.value.errors.push({startPosition: 1, endPosition: 2, startLineNumber: 1, endLineNumber: 1, message: e.message});
+        }
+        else {
+          throw e;
+        }
+      }
     }
 
     const checkSyntax = async () => {
@@ -161,7 +190,9 @@ export default defineComponent({
       })
     }
 
-    const handleChange: UploadProps['onChange'] = (uploadFile) => {
+    const handleChange: UploadProps['onChange'] = (uploadFile, uploadFiles) => {
+      if (uploadFiles.length > 1)
+        uploadFiles.shift();
       file.value = uploadFile
     }
 
@@ -176,16 +207,40 @@ export default defineComponent({
 
     const withLoader = async (func: () => Promise<any>) => {
       try {
+        isLoading.value = true;
         return await func();
       } finally {
         isLoading.value = false;
       }
     }
 
+    const modes = [
+      {
+        title: "Select tables",
+        pdfql: "select(tables)",
+        id: "tables"
+      },
+      {
+        title: "Select table rows",
+        pdfql: "select(tableRows)",
+        id: "tableRows"
+      },
+      {
+        title: "Select table cells",
+        pdfql: "select(tableCells)",
+        id: "tableCells"
+      },
+      {
+        title: "Write query manually (advanced)",
+        pdfql: "",
+        id: "manual"
+      },
+    ]
+
     const snippets = [
       {
         title: "Select 3 first tables",
-        pdfql: "select(tables)\r->take(3)"
+        pdfql: "select(tables)\r->take(3)",
       },
       {
         title: "Select table rows where first cell equals 'Customer'",
@@ -197,6 +252,16 @@ export default defineComponent({
       pdfql.value = value;
       isSnippetWindowOpened.value = false;
     }
+
+    const chooseSuggestion = (id: string) => {
+      currentModeId.value = id;
+      pdfql.value = currentMode.value!.pdfql;
+    }
+
+    const currentModeId = ref<string | null>(null);
+    const currentMode = computed(() => {
+      return currentModeId.value ? modes.find(m => m.id === currentModeId.value)! : null;
+    })
 
     watch(pdfql, () => {
       checkSyntax();
@@ -215,7 +280,11 @@ export default defineComponent({
       isSnippetWindowOpened,
       snippets,
       chooseSnippet,
-      handleChange
+      handleChange,
+      modes,
+      chooseSuggestion,
+      currentMode,
+      isLoading
     }
   }
 });
@@ -260,28 +329,15 @@ export default defineComponent({
   a:focus{
     color: #e2e3c2;
   }
-  .title-documentation-link{
-    margin-top: 20px;
-    margin-bottom: 60px;
-  }
   .pdf-uploader{
-    overflow: hidden;
-    cursor: pointer;
-    position: relative;
-    border: 1px dashed #fff;
-    width: 10vw;
-    height: 100%;
-    margin-left: 1vw;
-    display: flex;
-    justify-content: space-between;
-    flex-flow: column;
     padding: 2vw 0;
+    width: 15vw;
   }
-  .editor-frame-right{
+  .select-pdf-frame{
     display: flex;
-    justify-content: center;
     flex-direction: column;
-    background: #2D1639;
+    align-items: center;
+    justify-content: center;
   }
   .editor-code-frame{
     max-width: 871px;
@@ -291,6 +347,7 @@ export default defineComponent({
     background: #2D1639;
     display: flex;
     justify-content: space-between;
+    margin-top: 2vh;
   }
   .editor-frame-left{
     width: 100%;
@@ -389,5 +446,26 @@ export default defineComponent({
   .footer-step__description{
     margin-top: 5vh;
     font-size: 14px;
+  }
+  .snippet-selection-frame{
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    font-size: 18px;
+    margin-top: 5vh;
+  }
+  .snippet-selection-dropdown .el-button{
+    background: rgb(26, 4, 37);
+    color: #646547;
+    border: none;
+    text-decoration: underline;
+  }
+  .run-execution-frame{
+    margin-bottom: 5vh;
+  }
+</style>
+<style>
+  .el-upload-list__item:hover{
+    background-color: #2D1639 !important;
   }
 </style>
